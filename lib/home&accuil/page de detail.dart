7e78d1d05178c31c04bf023686213186edb85fdc
Marc William <marcwilliam756@gmail.com>
2023-07-04 08:37:14 +0100

    version 3.0.4.1.0

--- lib/home&accuil/page de detail.dart
+++ lib/home&accuil/page de detail.dart
@@ -1,4 +1,3 @@
-import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:vimeo_video_player/vimeo_video_player.dart';
@@ -10,6 +9,7 @@ import 'discussion.dart';
 class detail extends StatelessWidget{
 
   detail(maison this.item, {super.key});
+
   List imageCarousel = [
     "pathImage/1.jpeg",
     "pathImage/2.jpeg",
